3ba58f56e45b09d7a996336020d71f07835b16bf
Nicolas Gabriel Sanchez <nicosanchezdeveloper@gmail.com>
2023-09-29 16:05:47 -0300

    Actualizacion htaccess

--- app/home.php
+++ app/home.php
@@ -3,15 +3,13 @@
 require_once './app/db.php';
 function showHome(){
     include_once './templates/header.php';
-    echo "
-    
+    ?>
             <form action='agregar' method='POST'>
                 <input type='text' name='titulo_recordatorio' placeholder='titulo del recordatorio'>
                 <input type='text' name='descripcion_recordatorio' placeholder='descripcion del recordatorio'>
                 <button>agregar</button>
             </form>
-    ";
-
+    <?php
     $recordatorios = getRecordatorios();
     foreach ($recordatorios as $recordatorio) {
         echo $recordatorio->titulo_recordatorio;
